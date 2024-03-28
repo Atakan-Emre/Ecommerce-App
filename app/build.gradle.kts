7dd3290e5ac3ea3dfd8e7a8a3786e576fea97095
atakanemre <satakanemre@gmail.com>
2024-03-29 02:29:59 +0300

    Add build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+   // id("kotlin-kapt")
+  //  id("com.google.dagger.hilt.android")
+  //  id("com.google.devtools.ksp")
+
 }
 
 android {
@@ -9,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.ecommerce"
-        minSdk = 34
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -34,7 +39,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -47,9 +52,14 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+
 }
 
 dependencies {
+
+    val nav_version = "2.7.7"
+    val room_version = "2.6.1"
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -59,6 +69,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,4 +78,47 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Navigation Components
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    //Picaso
+    implementation ("com.squareup.picasso:picasso:2.8")
+
+    //View Pager2
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+
+    //Gson
+    implementation("com.squareup.retrofit2:converter-gson:2.10.1")
+    implementation("com.squareup.retrofit2:converter-moshi:2.10.1")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+
+    //okhttp
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    //mvvm
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+
+    //mvvm
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+
+    //dagger hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    //ksp("com.google.dagger:hilt-android-compiler:2.44")
+    //ksp("androidx.hilt:hilt-compiler:1.1.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    //room
+    implementation("androidx.room:room-runtime:$room_version")
+   // ksp("androidx.room:room-compiler:2.5.0")
+    implementation("androidx.room:room-ktx:$room_version")
+
+
 }
+
